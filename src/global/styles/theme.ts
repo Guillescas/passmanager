99ea56f0bfc952df418274ccbeb9857bd2261b6b
Guilherme Illescas <gui.illescas@gmail.com>
2021-06-20 22:48:08 -0300

    Create global style

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  colors: {
+    primary: '#4E3975',
+    secondary: '#9883BF',
+
+    pastel: '#FECBB2',
+
+    atention: '#D93025',
+
+    shape: '#FFF',
+
+    background: '#4E3975',
+    secondary_background: '#9883BF',
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+  },
+};
